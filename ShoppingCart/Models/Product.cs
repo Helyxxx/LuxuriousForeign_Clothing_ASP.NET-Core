ccf1d667c0ad38ce2d41f4ea6e7e0c2894c42090
Helyxxx <tmuslehuddin@hotmail.com>
2022-08-31 19:40:27 -0400

    minor changes

--- ShoppingCart/Models/Product.cs
+++ ShoppingCart/Models/Product.cs
@@ -6,6 +6,7 @@ namespace ShoppingCart.Models
     public class Product
     {
         public int ProductID { get; set; }
+
         [DisplayName("Product Name")]
         public string ProductName { get; set; }
         
@@ -13,6 +14,7 @@ namespace ShoppingCart.Models
         public decimal Price { get; set; }
         public string Category { get; set; }
         public string? Description { get; set; }
+
         [DisplayName("Photo")]
         public string? ImgUrl { get; set; }
     }

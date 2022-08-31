76d54c82c4076ea3cb4ebb52d86e2e52addfa69c
wzlwit <wangzl.wit@gmail.com>
2022-08-31 17:39:09 -0400

    add subTotal and format currency

--- ShoppingCart/Models/Product.cs
+++ ShoppingCart/Models/Product.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace ShoppingCart.Models
 {
@@ -7,6 +8,7 @@ namespace ShoppingCart.Models
         public int ProductID { get; set; }
         [DisplayName("Product Name")]
         public string ProductName { get; set; }
+        [DataType(DataType.Currency)]
         public decimal Price { get; set; }
         public string Category { get; set; }
         public string? Description { get; set; }
